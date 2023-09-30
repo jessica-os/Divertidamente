9217c20e86b69f469e7e60a64ff33a9d45ae2d6f
Jessica Oliveira <elisa0js@gmail.com>
2023-09-30 19:51:44 -0300

    melhorando tela dispositivos moveis

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 const personagens = document.querySelectorAll(".personagem");
 personagens.forEach((personagem) => {
   personagem.addEventListener("mouseenter", () => {
+    if (window.innerWidth < 450) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
     removerEadicionarSelecaoPersonagem(personagem);
     alterarImagemGrande(personagem);
     alterarNomePersonagem(personagem);
@@ -36,41 +39,51 @@ function alterarBolhaPersonagem(personagem) {
   const bolhasMedo = document.querySelector(".bolhas-medo");
   const bolhasTristeza = document.querySelector(".bolhas-tristeza");
 
-  const raiva = document.getElementById("raiva").addEventListener("mouseenter", () => {
-    bolhasMedo.style.display = "none";
-    bolhasTristeza.style.display = "none";
-    bolhasNojinho.style.display = "none";
-    bolhasAlegria.style.display = "none";
-    bolhasRaiva.style.display = "flex";
-  });
-  const alegria = document.getElementById("alegria").addEventListener("mouseenter", () => {
-    bolhasMedo.style.display = "none";
-    bolhasTristeza.style.display = "none";
-    bolhasNojinho.style.display = "none";
-    bolhasRaiva.style.display = "none";
-    bolhasAlegria.style.display = "flex";
-  });
-  const nojinho = document.getElementById("nojinho").addEventListener("mouseenter", () => {
-    bolhasMedo.style.display = "none";
-    bolhasTristeza.style.display = "none";
-    bolhasRaiva.style.display = "none";
-    bolhasAlegria.style.display = "none";
-    bolhasNojinho.style.display = "flex";
-  });
-  const tristeza = document.getElementById("tristeza").addEventListener("mouseenter", () => {
-    bolhasMedo.style.display = "none";
-    bolhasRaiva.style.display = "none";
-    bolhasAlegria.style.display = "none";
-    bolhasNojinho.style.display = "none";
-    bolhasTristeza.style.display = "flex";
-  });
-  const medo = document.getElementById("medo").addEventListener("mouseenter", () => {
-    bolhasRaiva.style.display = "none";
-    bolhasAlegria.style.display = "none";
-    bolhasNojinho.style.display = "none";
-    bolhasTristeza.style.display = "none";
-    bolhasMedo.style.display = "flex";
-  });
+  const raiva = document
+    .getElementById("raiva")
+    .addEventListener("mouseenter", () => {
+      bolhasMedo.style.display = "none";
+      bolhasTristeza.style.display = "none";
+      bolhasNojinho.style.display = "none";
+      bolhasAlegria.style.display = "none";
+      bolhasRaiva.style.display = "flex";
+    });
+  const alegria = document
+    .getElementById("alegria")
+    .addEventListener("mouseenter", () => {
+      bolhasMedo.style.display = "none";
+      bolhasTristeza.style.display = "none";
+      bolhasNojinho.style.display = "none";
+      bolhasRaiva.style.display = "none";
+      bolhasAlegria.style.display = "flex";
+    });
+  const nojinho = document
+    .getElementById("nojinho")
+    .addEventListener("mouseenter", () => {
+      bolhasMedo.style.display = "none";
+      bolhasTristeza.style.display = "none";
+      bolhasRaiva.style.display = "none";
+      bolhasAlegria.style.display = "none";
+      bolhasNojinho.style.display = "flex";
+    });
+  const tristeza = document
+    .getElementById("tristeza")
+    .addEventListener("mouseenter", () => {
+      bolhasMedo.style.display = "none";
+      bolhasRaiva.style.display = "none";
+      bolhasAlegria.style.display = "none";
+      bolhasNojinho.style.display = "none";
+      bolhasTristeza.style.display = "flex";
+    });
+  const medo = document
+    .getElementById("medo")
+    .addEventListener("mouseenter", () => {
+      bolhasRaiva.style.display = "none";
+      bolhasAlegria.style.display = "none";
+      bolhasNojinho.style.display = "none";
+      bolhasTristeza.style.display = "none";
+      bolhasMedo.style.display = "flex";
+    });
 
   const riley = document
     .getElementById("riley")
